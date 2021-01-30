c31ccdf6b2f391fb216432f308ec6b87384b5d59
valentinFouailly <valentin.fouailly@protonmail.com>
2021-01-30 10:59:39 +0100

    hello world

--- index.js
+++ index.js
@@ -1,104 +1,56 @@
 const Discord = require('discord.js');
-const client = new Discord.Client;
-const timers = require('timers')
-const config = require('./data/config.json')
-///////////////////////////////////////////////////
-const welcome_channel = config.welcome_channel               
-const emojiID = config.embed_emoji
-const logc = config.logchannel
-const activity1 = config.activity1
-const type1 = config.type1
-const activity2 = config.activity2
-const type2 = config.type2
-const time1 = config.activitytime
-const ownerID = config.ownerID
-const customtext = config.customtext
-//const etitle = config.embed_title
-const guildconfig = config.embed_guildname
-const ecolor = config.embed_color
-////////////////////////////////////////////////////
+const client = new Discord.Client();
+const config = require('./data/config.json');
 
-//Activites Const
-let activities = [
-  {
-    name:`${activity1}`,
-    options:{
-      type:`${type1}`
-    }
-  },
-  {
-    name:`${activity2}`,
-    options:{
-      type:`${type2}`
-    }
+const prefix = "!";
+
+var channel;
+
+function welcome(member, channel) {
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+}
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on("message", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
   }
-]
-let i = 0;
-//On Ready
-  client.on('ready', () => {
-    console.log(`${client.user.username} has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-    
-    timers.setInterval(() => {
-      i = i == activities.length ? 0 : i
-      client.user.setActivity(activities[i].name, activities[i].options)
-      i++
-    }, time1)
-  });
 
-  //On Message
-  client.on("message", async message =>{
-  if (message.content === "!add" && message.author.id === `${ownerID}`)
-  client.emit('guildMemberAdd', message.member);
-  if (message.content === "!remove" && message.author.id === `${ownerID}`)
-  client.emit('guildMemberRemove', message.member);
-
-  if (message.content === "!ping" && message.author.id === `${ownerID}`)
-  message.channel.send(`Pinging...!`).then(m => {
-    m.edit(
-      `:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` +
-        (m.createdTimestamp - message.createdTimestamp) +
-        `ms, Discord API heartbeat is ` +
-        Math.round(client.ping) +
-        `ms.`
-       );
-    });
- });
-
-  //On MemberAdd
-  client.on('guildMemberAdd', member => {
-  let count = member.guild.memberCount.toString() 
-  let end = count[count.length-1]
-  let suffixed = end == 1 ? count + "st" : end == 2 ? count + "nd" : end == 3 ? count + "rd" : count + "th" 
-  const channel = member.guild.channels.find(chnl => chnl.name === `${welcome_channel}`);
-  const memberavatar = member.user.displayAvatarURL
-      if (!channel) {
-        console.log("Set channel name in config.");
-        return;
-      };
-      const guildspot = guildconfig || member.guild
-      const emojispot = ` ` || `${emojiID}`
- //   let title1 = `${etitle}` || member.guild
-      let str = `Welcome to ${guildspot}! **${member.user.username}**! \nYou are the **${suffixed}** member!
-      \n${customtext} ${emojispot}`
-      const embed = new Discord.RichEmbed()
-      .setAuthor(member.user.tag, memberavatar)
-      .setColor(ecolor)
-  //  .setTitle(`${title1}`)
-      .setDescription(str)
-      .setThumbnail(memberavatar)
-      .setFooter(`📥 ${member.user.username} Joined!`)
-      .setTimestamp();
-      channel.send(embed);
-  
-  const logs = member.guild.channels.find(chnl => chnl.name === `${logc}`);
-  logs.send(`> :inbox_tray: ${member} has Joined ${member.guild.name}.`)
+  if (command === "welcome") {
+    member = message.author;
+    // Send the message to a designated channel on a server:
+    channel = message.channel;
+    channel.send(`This is now the welcome channel`);
+  }                        
 });
 
-//On MemerRemove
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+    console.log( member.displayName + " joined");
+    channel.send(`${member} est dans la place!`);
+});
+
+// Create an event listener for new guild members
 client.on('guildMemberRemove', member => {
-  const logs = member.guild.channels.find(chnl => chnl.name === `${logc}`);
-  logs.send(`> :outbox_tray: ${member} has left ${member.guild.name}.`)
+  // Send the message to a designated channel on a server:
+    console.log( member.displayName + " left");
+    channel.send(`Bye, ${member}!`);
 });
 
-//Log Client In
-client.login(config.token)
+client.login(config.BOT_TOKEN);
